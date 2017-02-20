7a2e75086cdf55da3e461496cd742c8bedfa89d2
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2017-02-20 18:14:42 +0000

    Adding new css to header

---
+++ PIU/UI7.php
@@ -0,0 +1,26 @@
+<?php
+include_once "common/header.html";
+?>
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Meetings
+                </h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+include_once "common/footer.html";
+?>
+
+
