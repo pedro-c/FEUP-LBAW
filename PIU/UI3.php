7cae77c782240d00f8fc3dfb724f7978e1781b19
Pedro Costa <pedrodc23@gmail.com>
2017-02-22 11:42:39 +0000

    css fixed

---
+++ PIU/UI3.php
@@ -0,0 +1,28 @@
+<?php
+include_once "common/header.html";
+?>
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Meetings
+                </h1>
+                <button type="button" class="btn btn-default navbar-btn">To-Do</button>
+                <button type="button" class="btn btn-default navbar-btn">Completed</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+include_once "common/footer.html";
+?>
+
+
