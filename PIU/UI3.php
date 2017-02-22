ec9adba4c156952eaac8a570219606181ef5fedf
Pedro Costa <pedrodc23@gmail.com>
2017-02-22 15:23:50 +0000

    improved css

--- PIU/UI3.php
+++ PIU/UI3.php
@@ -8,12 +8,47 @@ include_once "common/header.html";
 
         <!-- Page Heading -->
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    Meetings
-                </h1>
+            <div class="col-lg-12 text-center">
                 <button type="button" class="btn btn-default navbar-btn">To-Do</button>
                 <button type="button" class="btn btn-default navbar-btn">Completed</button>
+            </div>
+
+
+            <div class="col-lg-6">
+                <div class="navbar">
+                    <button class="btn-warning">Add Task</button>
+                </div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <button>Complete</button>
+                            </td>
+                            <td>
+                                <div>
+                                    <textarea>
+                                        Task1
+                                    </textarea>
+                                </div>
+                            </td>
+                            <td>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-lg-6">
+                <div class="navbar">
+                    <input type="text" placeholder="Assign to">
+                    <input type="date" name="deadline">
+                    <button class="btn-danger"> Delete </button>
+                </div>
+                <div>
+                    <input type="text" placeholder="New Task">
+                    <input type="text" placeholder="Description">
+                    <input type="text" placeholder="#project">
+                </div>
             </div>
         </div>
     </div>
