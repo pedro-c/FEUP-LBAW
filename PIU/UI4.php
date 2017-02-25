4cee88b60f8ebf559c04a757254a5d432f968ea7
epassos26 <epassos26@gmail.com>
2017-02-25 23:04:14 +0000

    some more progress

---
+++ PIU/UI4.php
@@ -0,0 +1,110 @@
+<?php include_once "common/header.html" ?>
+<!-- Custom CSS -->
+<link href="../css/forum.css" rel="stylesheet">
+<script src="../js/forum.js"></script>
+<div class="page-wrapper container">
+    <div class="row">
+        <br>
+        <div id="forum-content" class="list-group">
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">New Feature?</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                    <span class="submitter-uname">pedroc</span> -
+                    <span class="post-submission-date">Yesterday at 8:01</span>
+                    </small>
+                </div>
+            </span>
+            <div class="clearfix"></div>
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">Who did this? Like, seriously?!!</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                        <span class="submitter-uname">epassos</span> -
+                        <span class="post-submission-date">Feb 23 at 23:31</span>
+                    </small>
+                </div>
+            </span>
+            <div class="clearfix"></div>
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">Great PHP resource!</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                        <span class="submitter-uname">jccoutinho</span> -
+                        <span class="post-submission-date">Feb 24 at 12:18</span>
+                    </small>
+                </div>
+            </span>
+            <div class="clearfix"></div>
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">Deployment Instructions</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                        <span class="submitter-uname">mariajoaomp</span> -
+                        <span class="post-submission-date">Feb 12 at 15:12</span>
+                    </small>
+                </div>
+            </span>
+            <div class="clearfix"></div>
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">Some requirement changes..</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                        <span class="submitter-uname">pedroc</span> -
+                        <span class="post-submission-date">Jan 27 at 00:28</span>
+                    </small>
+                </div>
+            </span>
+            <div class="clearfix"></div>
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">Just a heads up!</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                        <span class="submitter-uname">epassos</span> -
+                        <span class="post-submission-date">Dec 21 at 8:01</span>
+                    </small>
+                </div>
+            </span>
+            <div class="clearfix"></div>
+            <span class="list-group-item forum-post">
+                <h4 class="list-group-item-heading">Random idea</h4>
+                <div class="list-group-item-text">
+                    <a class="submitter-pic glyphicon glyphicon-user"> </a>
+                    <small>
+                        <span class="submitter-uname">mariajoaomp</span> -
+                        <span class="post-submission-date">Dec 20 at 21:01</span>
+                    </small>
+                </div>
+            </span>
+            <nav aria-label="Page navigation">
+                <div class="text-center">
+                    <ul class="pagination">
+                        <li class="disabled">
+                            <a href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                        </li>
+                        <li class="active"><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+    </div>
+</div>
+<?php include_once "common/footer.html"?>
