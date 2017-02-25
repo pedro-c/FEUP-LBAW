d4d57792c02cd14460ed27aae0a7fcd3343d0fc0
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2017-02-25 17:59:38 +0000

    :v

--- js/ui1.js
+++ js/ui1.js
@@ -1,3 +1,23 @@
-/**
- * Created by mariajoaomirapaulo on 25/02/17.
- */
+
+
+
+function signUp(){
+    $(".form-login").hide();
+    $(".form-register").show();
+    $(".button_loginIn").css("background-color", "#425262");
+    $(".button_signUp").css("background-color", "#e9d460");
+    $(".sign-in-title").css("border","none");
+    $(".sign-up-title").css("border-bottom", "4px solid");
+    $(".sign-up-title").css("border-bottom-color", "#e9d460");
+}
+
+
+function signIn(){
+    $(".form-login").show();
+    $(".form-register").hide();
+    $(".button_signUp").css("background-color", "#425262");
+    $(".button_loginIn").css("background-color", "#e9d460");
+    $(".sign-up-title").css("border","none");
+    $(".sign-in-title").css("border-bottom", "4px solid");
+    $(".sign-in-title").css("border-bottom-color", "#e9d460");
+}
