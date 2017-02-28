e0cac7745b7cd65827d95acc4d86e79b5e007fcc
epassos26 <epassos26@gmail.com>
2017-02-28 20:14:30 +0000

    Initial version. Adding new files

---
+++ PIU/UI2.php
@@ -0,0 +1,92 @@
+<?php
+include_once 'common/header.html';
+?>
+<link href="../css/ui2.css" rel="stylesheet"/>
+<script src="../js/dashboard.js"></script>
+<div class="page-wrapper container">
+    <div class="row"><br>
+        <div id="project-presentation" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="page-header">
+                <h2 id="title">My Project</h2>
+                    <span id="subtitle">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa
+                        nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi
+                        cupiditate. Voluptatum ducimus voluptates voluptas?
+                    </span>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 dash-card">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title"><i class="glyphicon glyphicon-check"></i> Your Tasks</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">Implement feature</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">Merge changes</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">Fix conflicts</span></button>
+                    </div>
+                    <a href="#" class="to-page btn btn-default">To Tasks</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 dash-card">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title"><i class="fa fa-quote-right"></i> Forum</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">Don't forget to comment</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">Great JS resource</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">Some changes needed</span></button>
+                    </div>
+                    <a href="#" class="to-page btn btn-default">To Forum</a>
+                </div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 dash-card">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title"><i class="glyphicon glyphicon-calendar"></i> Meetings</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">ahoy</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">ahoy</span></button>
+                    </div>
+                    <a href="#" class="to-page btn btn-default">To Meetings</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 dash-card">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title"><i class="glyphicon glyphicon-file"></i> Files</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">ahoy</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">ahoy</span></button>
+                    </div>
+                    <a href="#" class="to-page btn btn-default">To Files</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 dash-card">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title"><i class="fa fa-users"></i> The Team</h4>
+                </div>
+                <div class="panel-body">
+                    <div class="list-group">
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">ahoy</span></button>
+                        <button class="list-group-item"><i class="glyphicon glyphicon-menu-right dash-icon"></i><span class="dash-item-text">ahoy</span></button>
+                    </div>
+                    <a href="#" class="to-page btn btn-default">To Team</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
