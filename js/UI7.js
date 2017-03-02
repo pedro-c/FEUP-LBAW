b8c6bf0546cd0dd0050397c790a4de0650b7ad47
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2017-03-02 16:52:33 +0000

    Small changes on mobile

--- js/UI7.js
+++ js/UI7.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
 function schedule() {
     $("#container_to_collapse").removeClass("col-lg-12 col-md-12 col-sm-12 col-xs-12");
-    $("#container_to_collapse").addClass("col-lg-6 col-md-6 col-sm-6 col-xs-6");
+    $("#container_to_collapse").addClass("col-lg-6 col-md-6 col-sm-6 hidden-xs");
     $(".meeting-panel").removeClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
     $(".meeting-panel").addClass("col-lg-12 col-md-12 col-sm-12 col-xs-12");
     $("#container_schedule_meeting").show();
