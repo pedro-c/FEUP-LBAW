fdfc47dc1fd0270030327befbf80e7acb3e832d6
Pedro Costa <pedrodc23@gmail.com>
2017-05-11 18:35:37 +0100

    Started profile pages

---
+++ final/database/profile.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pedroc
+ * Date: 11/05/17
+ * Time: 18:28
+ */
---
+++ final/pages/profile.php
@@ -0,0 +1,14 @@
+<?php
+include_once "common/header.php";
+include_once($BASE_DIR .'database/profile.php');
+
+
+$smarty->display($BASE_DIR . 'templates/profile.tpl');
+
+?>
+
+<?php
+include_once "common/footer.php";
+?>
+
+
