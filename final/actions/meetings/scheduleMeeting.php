f062b649dc60190280a3c95c524e8bf800683809
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2017-05-15 08:49:35 +0100

    Fixed some errors

--- final/actions/meetings/scheduleMeeting.php
+++ final/actions/meetings/scheduleMeeting.php
@@ -20,25 +20,26 @@ if(isset($_POST['title']) && isset($_POST['date']) && isset($_POST['time']) && i
     if(isset($_POST['tagOption']))
         addTagToMeeting($meeting_id,$_POST['tagOption']);
 
+    if($total > 1) {
+        for ($i = 0; $i < $total; $i++) {
+            echo $_FILES['file']['name'][$i];
 
-    for($i=0; $i<$total; $i++) {
-        echo $_FILES['file']['name'][$i];
+            if ($_FILES['file']['tmp_name'][$i] != "") {
+                $path = $BASE_DIR . 'images/files/';
+                $location = $path . $_FILES['file']['name'][$i];
 
-        if ($_FILES['file']['tmp_name'][$i] != "") {
-            $path = $BASE_DIR.'images/files/';
-            $location = $path . $_FILES['file']['name'][$i];
-
-            if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $location)) {
-                $_SESSION['success_messages'][] = '<br>'.'Uploaded File';
+                if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $location)) {
+                    $_SESSION['success_messages'][] = '<br>' . 'Uploaded File';
+                }
+                $_SESSION['success_messages'][] = '<br>' . 'Error on uploading File';
             }
-            $_SESSION['success_messages'][] = '<br>'.'Error on uploading File';
-        }
-        $current_date = date('m/d/Y h:i:s a', time());
-        $file = addFile($current_date, $_SESSION['user_id'],$_SESSION['project_id'], $_FILES['file']['name'][$i]);
-        addFileToMeeting($file, $meeting_id);
+            $current_date = date('m/d/Y h:i:s a', time());
+            $file = addFile($current_date, $_SESSION['user_id'], $_SESSION['project_id'], $_FILES['file']['name'][$i]);
+            addFileToMeeting($file, $meeting_id);
 
-        if(isset($_POST['tagOption']))
-            addTagToFile($file,$_POST['tagOption']);
+            if (isset($_POST['tagOption']))
+                addTagToFile($file, $_POST['tagOption']);
+        }
     }
 
 
--- final/javascript/UI7.js
+++ final/javascript/UI7.js
@@ -35,6 +35,7 @@ function deleteMeeting() {
         dataType: 'json',
         success: function (data) {
             console.log(data);
+            location.reload();
         }
     });
 }
