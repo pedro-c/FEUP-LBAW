188887ef8dc534585a2f1f7f307d425052a79bcb
José Carlos Coutinho <jczelik@gmail.com>
2017-05-22 11:58:07 +0100

    invite member working and generating a random code

--- final/database/team.php
+++ final/database/team.php
@@ -47,8 +47,54 @@ function removeMember($user_id, $project_id) {
 function inviteMember($user_email, $project_id) {
 
     global $conn;
-    $sql_invite_member = "INSERT INTO invited_users (id_project, email) VALUES (?, ?);";
+    $invited_member = getInvitedMember($user_email, $project_id);
+    $generated_code = generateInviteCode($user_email, $project_id);
+    if($invited_member != null || $generated_code == null) {
+      return null;
+    }
+    $sql_invite_member = "INSERT INTO invited_users (id_project, email, code) VALUES (?, ?, ?);";
     $stmt = $conn->prepare($sql_invite_member);
-    return $stmt->execute(array($project_id, $user_email));
+    return $stmt->execute(array($project_id, $user_email, $generated_code));
+}
+
+function getInvitedMember($user_email, $project_id) {
+
+    global $conn;
+    $sql_select_invited_member = "SELECT id_project, email FROM invited_users WHERE id_project = ? AND email = ?;";
+    $stmt = $conn->prepare($sql_select_invited_member);
+    $stmt->execute(array($project_id, $user_email));
+    return $stmt->fetch();
+}
+
+function generateInviteCode($user_email, $project_id) {
+
+    global $conn;
+
+    $length = 10;
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    $sql_select_code = "SELECT * FROM invited_users WHERE code = ?;";
+    $stmt = $conn->prepare($sql_select_code);
+    $stmt->execute(array($randomString));
+    $a = $stmt->fetch();
+    if($a != null) {
+      return null;
+    } else {
+      return $randomString;
+    }
+}
+
+function invitedUserExists($user_email, $project_id, $code) {
+
+    global $conn;
+    $sql_select_invited_member = "SELECT * FROM invited_users WHERE email = ? AND id_project = ? AND code = ?;";
+    $stmt = $conn->prepare($sql_select_invited_member);
+    $stmt->execute(array($user_email, $project_id, $code));
+    return $stmt->fetch();
 }
 ?>
